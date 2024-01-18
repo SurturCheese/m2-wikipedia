0ba1294abb3a1e2499903d674b85ef0b3997c626
Tommy Goc <tommy.goc@outlook.fr>
2024-01-18 10:33:36 +0100

    refactor and correct double qote

--- wikiSearch/search/views.py
+++ wikiSearch/search/views.py
@@ -14,7 +14,7 @@ def index(request):
 
     start_time = time.time()
 
-    df = parse_and_call_function(input_string, settings.SPARK, result_table)
+    df = parse_and_execute_commands(input_string, settings.SPARK, result_table)
 
     if input_string:
         if 'COUNT' in input_string:
@@ -22,7 +22,6 @@ def index(request):
         elif 'DATASET' in input_string and not ('TITLE' in input_string or 'CONTAINS' in input_string or 'CATEGORY' in input_string):
             pass
         else:
-            print("je suis ")
             df.collect()
 
     context = {"table": result_table}
@@ -38,7 +37,7 @@ def index(request):
             existing_data.append(data)
         with open('result.json', 'w') as f:
             json.dump(existing_data, f)
-    else:
+    else:   
         with open('result.json', 'w') as f:
             json.dump([data], f)
 
@@ -46,36 +45,54 @@ def index(request):
 
     return render(request, 'searchTemplate/index.html', context)
 
-def parse_and_call_function(input_string, spark, result_table):
+def parse_and_execute_commands(input_string, spark, result_table):
     if not input_string:
         return spark.read.parquet(f"wikiSearch/search/data/{DEFAULT_DATASET}")
 
-    words = re.findall(r'(\S+)', input_string)
-    
+    words = re.findall(r'(?:[^\s"]|"(?:\\.|[^"])*")+', input_string)
+
     if len(words) > 0:
-        if words[0].upper() == "SET":
-            result = set_dataset(words[2])
-            if len(words) == 3:
-                result_table.add_row(result)
-            if len(words) > 3:
-                return parse_and_execute_additional_commands(words[3:], spark)
-        elif words[0].upper() == "TITLE" and len(words) > 1:
-            print("TITLE")
-            phrase = extract_quoted_phrase(input_string)
-            return execute_search_command(search_title, phrase, spark)
-        elif words[0].upper() == "CATEGORY" and len(words) > 1:
-            print("CATEGORY")
-            return execute_search_command(search_category, words[1], spark)
-        elif words[0].upper() == "CONTAINS" and len(words) > 1:
-            print("CONTAINS")
-            phrase = extract_quoted_phrase(input_string)
-            return execute_search_command(contains, phrase, spark)
-        else:
-            result_table.add_row(["Invalid command"])
-    else:
-        result_table.add_row(["Empty input string"])
+        i = 0
+        while i < len(words):
+            command = words[i].upper()
+            print(command)
+            if command == "SET":
+                result = set_dataset(words[i + 2])
+                if len(words) > i + 3 and words[i + 3].upper() in ["TITLE", "CATEGORY", "CONTAINS"]:
+                    i += 3
+                    continue
+                elif len(words) == i + 3:
+                    result_table.add_row(result)
+            elif command == "TITLE" and i + 1 < len(words):
+                print("TITLE")
+                # Check if the phrase is enclosed in double quotes
+                if words[i + 1].startswith('"') and words[i + 1].endswith('"'):
+                    phrase = words[i + 1][1:-1]
+                    return execute_search_command(search_title, phrase, spark)
+                else:
+                    result_table.add_row(["Invalid TITLE command: Phrase should be enclosed in double quotes"])
+            elif command == "CATEGORY" and i + 1 < len(words):
+                print("CATEGORY")
+                # Check if the phrase is enclosed in double quotes
+                if words[i + 1].startswith('"') and words[i + 1].endswith('"'):
+                    category = words[i + 1][1:-1]
+                    return execute_search_command(search_category, category, spark)
+                else:
+                    result_table.add_row(["Invalid CATEGORY command: Phrase should be enclosed in double quotes"])
+            elif command == "CONTAINS" and i + 1 < len(words):
+                print("CONTAINS")
+                # Check if the phrase is enclosed in double quotes
+                if words[i + 1].startswith('"') and words[i + 1].endswith('"'):
+                    phrase = words[i + 1][1:-1]
+                    return execute_search_command(contains, phrase, spark)
+                else:
+                    result_table.add_row(["Invalid CONTAINS command: Phrase should be enclosed in double quotes"])
+            else:
+                result_table.add_row(["Invalid command"])
+            i += 1
 
     return spark.read.parquet(f"wikiSearch/search/data/{DEFAULT_DATASET}")
+
 
 def extract_quoted_phrase(input_string):
     match = re.search(r'"([^"]*)"', input_string)
@@ -90,27 +107,12 @@ def set_dataset(dataset_name):
     except Exception as e:
         return [f"Error changing dataset to {DEFAULT_DATASET}", str(e)]
 
-def parse_and_execute_additional_commands(commands, spark):
-    for i in range(0, len(commands), 2):
-        command = commands[i].upper()
-        if command == "TITLE" and i + 1 < len(commands):
-            print("TITLE")
-            phrase = extract_quoted_phrase(commands[i + 1])
-            return execute_search_command(search_title, phrase, spark)
-        elif command == "CATEGORY" and i + 1 < len(commands):
-            print("CATEGORY")
-            return execute_search_command(search_category, commands[i + 1], spark)
-        elif command == "CONTAINS" and i + 1 < len(commands):
-            print("CONTAINS")
-            phrase = extract_quoted_phrase(commands[i + 1])
-            return execute_search_command(contains, phrase, spark)
-    return spark
-
 def execute_search_command(search_function, argument, spark):
     df = spark.read.parquet(f"wikiSearch/search/data/{DEFAULT_DATASET}")
     return search_function(df, argument)
 
 def search_title(df, title):
+    print('title : ' + title)
     return df.filter(df["title"].contains(title))
 
 def search_category(df, category):
